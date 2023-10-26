1af88696868195d540ee8e3d051aaa0695e3e5e5
Shivam2285 <shoyamato2285@gmail.com>
2023-10-26 15:39:07 +0530

    lol

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i;
+printf("\n Enter any Number:");
+scanf("%d",&a);
+i=1;
+i++;
+b=a+i;
+printf("\n The next 10 numbers after entered number are \n %d",b);
+
+}
+
+
